23e921a956f20ca49e8d831d8253bd538197c26e
Shabbeer Hassan <shabbeer.hassan@helsinki.fi>
2018-12-10 01:22:39 +0200

    week6

--- meet_and_repeat.R
+++ meet_and_repeat.R
@@ -41,3 +41,6 @@ write.table(BPRSL, "~/Documents/GitHub/IODS-project/data/BPRSL.txt",
             sep = "\t", row.names = FALSE, quote = FALSE) 
 write.table(RATSL, "~/Documents/GitHub/IODS-project/data/RATSL.txt", 
             sep = "\t", row.names = FALSE, quote = FALSE) 
+write.csv(BPRSL,"data/BPRSL.csv", row.names=FALSE)
+write.csv(RATSL,"data/RATSL.csv", row.names=FALSE)
+write.csv(BPRS,"data/BPRS.csv", row.names=FALSE)
